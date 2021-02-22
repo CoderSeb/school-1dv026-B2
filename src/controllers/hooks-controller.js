a36712b7d85b7b9afe8458e23de5a88287063491
Sebastian <sa224ny@student.lnu.se>
2021-02-22 15:29:00 +0100

    Added: Method to close an issue

--- src/controllers/hooks-controller.js
+++ src/controllers/hooks-controller.js
@@ -6,7 +6,7 @@
  */
 
 import moment from 'moment'
-
+import axios from 'axios'
 /**
  * Encapsulates a controller.
  */
@@ -35,9 +35,11 @@ export class HooksController {
       const issueObj = {
         title: issue.object_attributes.title,
         id: issue.object_attributes.iid,
+        masterId: issue.object_attributes.id,
         state: issue.object_attributes.state,
+        isOpen: issue.object_attributes.state === 'opened',
         description: issue.object_attributes.description,
-        creator: issue.user.name,
+        creator: issue.object_attributes.author_id === 558 ? 'Sebastian Åkerblom' : issue.user.name,
         created: moment(new Date(issue.object_attributes.created_at)).fromNow(),
         updated: moment(new Date(issue.object_attributes.updated_at)).fromNow(),
         labels: issue.object_attributes.labels
@@ -46,4 +48,25 @@ export class HooksController {
       res.status(200).send('New information from socket!')
     }
   }
-}
+
+  /**
+   * Sends a request to gitlab to close an issue.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async issueClose (req, res, next) {
+    axios({
+      method: 'PUT',
+      url: `${process.env.GITLAB_URL}/${process.env.GITLAB_PROJECT_ID}/issues/${req.params.id}?state_event=close`,
+      headers: {
+        Authorization: 'Bearer ' + process.env.GITLAB_TOKEN
+      }
+    }).then(response => {
+      res.status(200).send('Issue is being closed...')
+    }).catch(err => {
+      console.log('Ops! Something went wrong! ' + err.message)
+      res.redirect('./')
+    })
+  }
